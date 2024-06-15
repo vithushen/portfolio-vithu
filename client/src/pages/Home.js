3a88c2daa4f160fac7092e440b4937ed29f53c4e
vithushen <vithu.99@hotmail.com>
2024-06-15 17:32:57 -0400

    [4]-Finished with about me section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import vithuImage from '../images/vithu.png';
 import vithuImage2 from '../images/vithu-image.png';
 
@@ -32,6 +32,14 @@ function Home() {
         return words.length;
     };
 
+    const aboutMeRef = useRef(null);
+
+    // Function to scroll to About Me section
+    const scrollToAboutMe = () => {
+        aboutMeRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
+
     return (
         <div className='bg-gray-900 text-white'>
             {/* Navbar */}
@@ -50,7 +58,7 @@ function Home() {
                 <div className="hidden sm:flex sm:items-center space-x-8"> {/* Added space-x-8 for spacing between links */}
                     <ul className="menu menu-horizontal text-lg space-x-8"> {/* Increased font size */}
                         <li><a>Home</a></li>
-                        <li><a>About Me</a></li>
+                        <li><a onClick={scrollToAboutMe}>About Me</a></li> {/* Added onClick handler */}
                         <li><a>Skills</a></li>
                         <li><a>My Projects</a></li>
                         <li><a href="https://www.linkedin.com/in/vithushen-sivasubramaniam/">LinkedIn</a></li>
@@ -64,11 +72,11 @@ function Home() {
                 <div className="sm:hidden flex justify-center mt-4"> {/* Added mt-4 for top margin */}
                     <ul className="menu menu-vertical text-lg">
                         <li><a className="text-white block py-2 hover:bg-gray-800 bg-info">Home</a></li>
-                        <li><a className="text-info block py-2 hover:bg-gray-800">About Me</a></li>
+                        <li><a className="text-info block py-2 hover:bg-gray-800" onClick={scrollToAboutMe}>About Me</a></li> {/* Added onClick handler */}
                         <li><a className="text-info block py-2 hover:bg-gray-800">Skills</a></li>
                         <li><a className="text-info block py-2 hover:bg-gray-800">My Projects</a></li>
-                        <li><a className="text-info block py-2 hover:bg-gray-800" href="https://www.linkedin.com/in/vithushen-sivasubramaniam/">LinkedIn</a></li> 
-                        <li><a className="text-info block py-2 hover:bg-gray-800" href="https://github.com/vithushen">GitHub</a></li> 
+                        <li><a className="text-info block py-2 hover:bg-gray-800" href="https://www.linkedin.com/in/vithushen-sivasubramaniam/">LinkedIn</a></li>
+                        <li><a className="text-info block py-2 hover:bg-gray-800" href="https://github.com/vithushen">GitHub</a></li>
                     </ul>
                 </div>
             )}
@@ -95,7 +103,7 @@ function Home() {
                         <div className="mt-4">
                             <button className="btn btn-info mr-2" onClick={() => document.getElementById('my_modal_3').showModal()}>Contact Me</button>
                             <dialog id="my_modal_3" className="modal">
-                                <div className="modal-box bg-gray-900 p-8 rounded-lg">
+                                <div className="modal-box bg-gray-900 p-8 rounded-lg ">
                                     <form onSubmit={sendEmail}>
                                         <button className="btn btn-sm btn-circle btn-ghost absolute top-2 right-2" onClick={() => document.getElementById('my_modal_3').close()}>✕</button>
                                         <h1 className="text-3xl font-bold mb-4 text-center">Send a Message to Vithu!</h1>
@@ -134,39 +142,47 @@ function Home() {
             </div>
 
             {/* About Me Section */}
-            <div className="hero min-h-screen bg-gray-800 text-white">
+            <div ref={aboutMeRef} className="hero min-h-screen bg-gray-800 text-white">
                 <div className="hero-content flex-col lg:flex-row">
-                    <img src={vithuImage2} className="rounded-lg shadow-2xl w-96 h-96 lg:w-[60rem] lg:h-[60rem]" alt="About Me" />
-                    <div>
-                        <h1 className="text-5xl font-bold">About Me</h1>
-                        <h1 className="text-5xl font-bold">____________</h1>
+                    <img
+                        src={vithuImage2}
+                        className="rounded-lg shadow-2xl w-72 h-73 lg:w-96 lg:h-full mt-20"
+                        style={{
+                            boxShadow: '0 2px 8px rgba(56, 189, 248, 0.9)', // Replace with the shadow color you want
+                            marginRight: '2rem' // Add margin to the right of the image
+                        }}
+                        alt="About Me"
+                    />
+                    <div className="lg:ml-8"> {/* Added margin-left to the text container */}
+                        <h1 className="text-5xl font-bold text-center">About Me</h1>
+                        <h1 className="text-5xl font-bold text-center">____________</h1>
                         <div className="flex flex-wrap justify-center lg:justify-start w-full mt-12 gap-5">
                             <div className="grid h-20 card bg-info rounded-box place-items-center text-black w-[175px]">
                                 <p className='font-bold'>Expertise</p>
-                                <p className="text-sm">Software Development</p>
+                                <p className="text-sm text-white">Software Development</p>
                             </div>
                             <div className="grid h-20 card bg-info rounded-box place-items-center text-black w-[175px]">
                                 <p className='font-bold'>Location</p>
-                                <p className="text-sm">Montreal, Qc</p>
+                                <p className="text-sm text-white">Montreal, Qc</p>
                             </div>
                             <div className="grid h-20 card bg-info rounded-box place-items-center text-black w-[175px]">
                                 <p className='font-bold'>Education</p>
-                                <p className="text-sm">Concordia University</p>
+                                <p className="text-sm text-white">Concordia University</p>
                             </div>
                             <div className="grid h-20 card bg-info rounded-box place-items-center text-black w-[175px]">
                                 <p className='font-bold'>Graduation</p>
-                                <p className="text-sm">June 2023</p>
+                                <p className="text-sm text-white">June 2023</p>
                             </div>
                         </div>
 
-                        <p className="py-6">After 5 years of programming, I have learned a lot in frontend and backend development, particularly in React with proficiency in HTML, CSS, JavaScript, Java, Python and SQL. I have a portfolio of user-friendly, responsive websites, showcasing my dedication to creating user experiences. Collaborating within teams of various sizes, I have successfully worked on projects using Agile and Scrum methodologies, demonstrating my ability to work in a collaborative and fast-paced environment using Github and Jira.</p>
+                        <p className="py-6 text-center lg:text-left">After 5 years of programming, I have learned a lot in frontend and backend development, particularly in React with proficiency in HTML, CSS, JavaScript, Java, Python and SQL. I have a portfolio of user-friendly, responsive websites, showcasing my dedication to creating user experiences. Collaborating within teams of various sizes, I have successfully worked on projects using Agile and Scrum methodologies, demonstrating my ability to work in a collaborative and fast-paced environment using Github and Jira.</p>
 
-                        <p className="py-0">I have also received a certification in the Foundation of SRE from Wiley Edge Academy. This certification has equipped me with advanced skills in executing Production Support and Site Reliability Engineering responsibilities. Utilizing tools such as Kubernetes, Grafana, and Prometheus, I've gained experience in monitoring website performance to ensure optimal operation and reliability. Moreover, I've implemented automation strategies to reduce manual tasks and toil, thereby enhancing efficiency and reliability in website operations. Furthermore, I've developed robust testing procedures using Jenkins, automating testing processes to maintain code quality and reliability throughout the development lifecycle.</p>
+                        <p className="py-0 text-center lg:text-left">I have also received a certification in the Foundation of SRE from Wiley Edge Academy. This certification has equipped me with advanced skills in executing Production Support and Site Reliability Engineering responsibilities. Utilizing tools such as Kubernetes, Grafana, and Prometheus, I've gained experience in monitoring website performance to ensure optimal operation and reliability. Moreover, I've implemented automation strategies to reduce manual tasks and toil, thereby enhancing efficiency and reliability in website operations. Furthermore, I've developed robust testing procedures using Jenkins, automating testing processes to maintain code quality and reliability throughout the development lifecycle.</p>
 
-                        <button className="btn btn-info mt-5">Get Started</button>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 }
